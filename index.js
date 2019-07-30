fe1c62f96ce6b654e21639ce4b887d54c8b5be91
Agon Cecelia <agonn.c@gmail.com>
2019-07-30 19:41:51 +0200

    Implement user module fully

--- index.js
+++ index.js
@@ -55,9 +55,59 @@ app.delete('/user/:id', (req, res, next) => {
         res.json(result);
     })
 })
+//Update user by id
+app.put('/user/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate(id, {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    }, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json(result);
+    })
+})
+//login
+app.post('/user/login', (req, res) => {
+    let userEmail = req.body.email;
+    let userPassword = req.body.password;
+
+    UserModel.findOne({email: userEmail}, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        if(result == null) {
+            res.json({
+                message: "User does not exist",
+                loggedIn: false
+            })
+            return;
+        }
+
+        if (userPassword == result.password) {
+            res.json({
+                message: 'Logged in successfully',
+                loggedIn: true
+            })
+            return;
+        }
+        res.json({
+            message: 'Email or password incorrect',
+            loggedIn: false
+        })
+    })
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/test', (req, res) => res.json({
     message: "Cohuni o njerz"
 }))
 
+// Start the server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
